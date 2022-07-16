fa0c127cbd1efa426687cc012b2b981a694d9824
8bNFT <92310321+8bNFT@users.noreply.github.com>
2022-07-16 23:40:06 +0200

    cache L1 balance because stingy

--- src/util/blockchain.js
+++ src/util/blockchain.js
@@ -19,6 +19,8 @@ const CONTRACT_ABI = {
     ]
 }
 
+const balanceCache = {}
+
 export const buildWeb3 = network => {
     network = network || Wallet.getNetwork()
     if(!network) throw "No network specified (mainnet|testnet)"
@@ -26,19 +28,44 @@ export const buildWeb3 = network => {
     return new Web3(RPC_ENDPOINTS[network])
 }
 
+const getBalanceCacheKey = ({ wallet, token, network }) => `${wallet.toLowerCase()}|${token && token.id + "|" || ""}${network}`
+
+const getBalanceCache = ({ wallet, token, network }) => {
+    const key = getBalanceCacheKey({ wallet, token, network })
+    const cache = balanceCache[key]
+    if(cache && cache.expires < Date.now()) return cache.data
+    return false
+}
+
+const setBalanceCache = ({ wallet, token, network, data, expires = 60}) => {
+    const key = getBalanceCacheKey({ wallet, token, network })
+    balanceCache[key] = { data, expires: Date.now() + (expires * 1000)}
+    return data
+}
+
 export const getERC20Balance = async ({ wallet, token, network }) => {
+    const cache = getBalanceCache({ wallet, token, network })
+    if(cache) return cache
+
     const web3 = buildWeb3(network)
     const ABI = CONTRACT_ABI.ERC20
-
     const contract = new web3.eth.Contract(ABI, token.token_address)
+
     const balance = await contract.methods.balanceOf(wallet).call()
-    return parseWithDecimals(balance, token.decimals, token.precision)
+    const parsedBalance = parseWithDecimals(balance, token.decimals, token.precision)
+
+    return setBalanceCache({ data: parsedBalance, wallet, token, network })
 }
 
 export const getETHBalance = async ({ wallet, network }) => {
+    const cache = getBalanceCache({ wallet, network })
+    if(cache) return cache
+
     const web3 = buildWeb3(network)
     const balance = await web3.eth.getBalance(wallet)
-    return parseWithDecimals(balance, 18, 10)
+    const parsedBalance = parseWithDecimals(balance, 18, 10)
+    
+    return setBalanceCache({ wallet, network, data: parsedBalance })
 }
 
 export const getTokenBalance = ({ wallet, token, network }) => {
